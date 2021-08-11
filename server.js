ad3b2d7a4e0e35f31d852e17b37c1fb72218fac3
sahilthakur7 <sahilrd07@gmail.com>
2021-08-11 17:06:03 +0530

    Added graphiql and complete player schema

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const graphqlHTTP = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./src/schema/schema');
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({}));
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+	graphiql: true
+  })
+);
 
 app.listen(4000, () => {
-	console.log("Listening on port 4000")
+  console.log('Listening on port 4000');
 });
--- src/schema/schema.js
+++ src/schema/schema.js
@@ -1,12 +1,61 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
-const { GraphQLOjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
 
-const PlayerType = new GraphQLOjectType({
-	name: 'Player',
-	fields: () => ({
-		id: { type: GraphQLString },
-		name: { type: GraphQLString },
-		position: { type: GraphQLString}
-	}),
+//Dummy data
+
+const players = [
+  { id: '1', name: 'Scott McTominay', position: 'CDM' },
+  { id: '2', name: 'Bruno Fernandes', position: 'CAM' },
+  { id: '3', name: 'Jadon Sancho', position: 'RWM' },
+  { id: '4', name: 'Memphis', position: 'RWM' },
+];
+
+const teams = [
+  { id: '1', name: 'Man Utd', country: 'England' },
+  { id: '2', name: 'Barcelona', country: 'Spain' },
+  { id: '3', name: 'Schalke', country: 'Germany' },
+];
+
+const PlayerType = new GraphQLObjectType({
+  name: 'Player',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    position: { type: GraphQLString },
+  }),
+});
+
+const TeamType = new GraphQLObjectType({
+  name: 'Team',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    country: { type: GraphQLString },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    player: {
+      type: PlayerType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(players, { id: args.id });
+      },
+    },
+    team: {
+      type: TeamType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(teams, { id: args.id });
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
